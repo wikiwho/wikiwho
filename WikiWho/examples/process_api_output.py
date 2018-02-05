c6f832b8b44d8b85666bc5dfc1c0cdb6334a0298
Kenan Erdogan <kenan.erdogan@gesis.org>
2018-02-05 10:50:33 +0100

    fix examples

--- WikiWho/examples/process_api_output.py
+++ WikiWho/examples/process_api_output.py
@@ -1,11 +1,26 @@
 import requests
 
-from wikiwho.wikiwho import Wikiwho
-from wikiwho.utils import iter_rev_tokens
+from WikiWho.wikiwho import Wikiwho
 
 
 def process_api_output(page_id):
-    # you check here for the explanation of the api call
+    """
+    Example usage:
+
+    from WikiWho.examples.process_api_output import process_api_output
+    from WikiWho.utils import iter_rev_tokens
+
+    page_id = 6187
+    wikiwho_obj = process_api_output(page_id)
+    print(wikiwho_obj.title)
+    print(wikiwho_obj.ordered_revisions)
+    for token in iter_rev_tokens(wikiwho_obj.revisions[wikiwho_obj.ordered_revisions[0]]):
+        print(token.value, token.token_id, token.origin_rev_id)
+
+    :param page_id: Page id of the article which is  going to be analysed.
+    :return: WikiWho object.
+    """
+    # you can check here for the explanation of the api call
     # https://www.mediawiki.org/wiki/API:Revisions
     url = 'https://en.wikipedia.org/w/api.php'
     params = {'pageids': page_id, 'action': 'query', 'prop': 'revisions',
@@ -22,15 +37,10 @@ def process_api_output(page_id):
         raise Exception('The article ({}) you are trying to request does not exist!'.format(page_id))
 
     _, page = result['query']['pages'].popitem()
+    if 'missing' in page:
+        raise Exception('The article ({}) you are trying to request does not exist!'.format(page_id))
+
     wikiwho = Wikiwho(page['title'])
     wikiwho.analyse_article(page.get('revisions', []))
     wikiwho.rvcontinue = result['continue']['rvcontinue']
     return wikiwho
-
-if __name__ == '__main__':
-    page_id = 6187
-    wikiwho_obj = process_api_output(page_id)
-    print(wikiwho_obj.title)
-    print(wikiwho_obj.ordered_revisions)
-    for token in iter_rev_tokens(wikiwho_obj.revisions[wikiwho_obj.ordered_revisions[0]]):
-        print(token.value, token.token_id, token.origin_rev_id)
--- WikiWho/examples/process_xml_dump.py
+++ WikiWho/examples/process_xml_dump.py
@@ -1,10 +1,25 @@
 from mwxml import Dump
 from mwtypes.files import reader
 
-from wikiwho.wikiwho import Wikiwho
+from WikiWho.wikiwho import Wikiwho
 
 
 def process_xml_dump(xml_file_path):
+    """
+    Link to xml dumps: https://dumps.wikimedia.org/enwiki/
+
+    Example usage:
+
+    from WikiWho.examples.process_xml_dump import process_xml_dump
+
+    xml_file_path = '/home/kenan/Downloads/enwiki-20180101-pages-meta-history1.xml-p5753p7728.7z'
+    wikiwho_obj = process_xml_dump(xml_file_path)
+    print(wikiwho_obj.title)
+    print(wikiwho_obj.ordered_revisions)
+
+    :param xml_file_path:
+    :return: WikiWho object.
+    """
     # more info about reading xml dumps: https://github.com/mediawiki-utilities/python-mwxml
     dump = Dump.from_file(reader(xml_file_path))
     for page in dump:
@@ -12,10 +27,3 @@ def process_xml_dump(xml_file_path):
         wikiwho.analyse_article_from_xml_dump(page)
         break  # process only first page
     return wikiwho
-
-if __name__ == '__main__':
-    # link to xml dumps: https://dumps.wikimedia.org/enwiki/
-    xml_file_path = 'path/to/xml'
-    wikiwho_obj = process_xml_dump(xml_file_path)
-    print(wikiwho_obj.title)
-    print(wikiwho_obj.ordered_revisions)
